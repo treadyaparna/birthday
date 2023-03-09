533159d096f010966710819f165e535ee3b387c8
Aparna Saha <tready.aparna@gmail.com>
2023-03-09 01:49:01 +0100

    test: add tests

---
+++ Tests/Controller/BirthdayControllerTest.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Tests\Controller;
+
+require_once("./src/Controller/BirthdayController.php");
+
+
+use PHPUnit\Framework\TestCase;
+use Birthday\Controller\BirthdayController;
+
+class BirthdayControllerTest extends TestCase
+{
+    
+}
---
+++ Tests/Service/JsonDataServiceTest.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Tests\Service;
+
+class JsonDataServiceTest
+{
+    
+}
